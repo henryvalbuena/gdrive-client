ade7b8a32a1a8425485e5bf448f37170238de386
Henry Valbuena <dev.henryvm@gmail.com>
2019-11-16 19:13:27 -0500

    WIP: Implemented File model

--- db_interface.py
+++ db_interface.py
@@ -1,21 +1,56 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, \
-    Integer, String
-
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///fileids.db', echo=True)
-meta = MetaData()
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
-files = Table(
-    'files', meta,
-    Column('id', Integer, primary_key=True),
-    Column('fileid', String, unique=True),
-    Column('filename', String),
-    Column('filesize', Integer),
+
+class File(Base):
+    __tablename__ = 'files'
+
+    id = Column('id', Integer, primary_key=True)
+    fileid = Column('fileid', String, unique=True)
+    filename = Column('filename', String)
+    filesize = Column('filesize', Integer)
+
+
+Base.metadata.create_all(engine)
+
+# Example data
+file_list = [
+    {
+        'fileid': '78',
+        'filename': 'tests.txt',
+        'filesize': 12
+    },
+    {
+        'fileid': '21',
+        'filename': 'test_a.txt',
+        'filesize': 11
+    },
+    {
+        'fileid': '3',
+        'filename': 'test0.txt',
+        'filesize': 22
+    },
+    {
+        'fileid': '2',
+        'filename': 'texts.txt',
+        'filesize': 50
+    },
+]
+
+session.add_all(
+    [
+        File(
+            fileid=fl['fileid'],
+            filename=fl['filename'],
+            filesize=fl['filesize'])
+        for fl in file_list
+    ]
 )
-meta.create_all(engine)
-insert = files.insert().values(
-    fileid='12345',
-    filename='test1.txt',
-    filesize=999)
-conn = engine.connect()
-result = conn.execute(insert)
+
+session.commit()
